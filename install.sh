a880c1008c2e283198e7935ba36be21f3ef6eaa3
Zac D <zdelesde@gmail.com>
2024-10-01 18:33:32 -0400

    updates

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
-mkdir -p /var/lib/zcheck /var/lib/packages
+install -o root -g root -m 755 -d /var/lib/zcheck
+install -o root -g root -m 755 -d /var/lib/packages
 install -o root -g root -m 700 zcheck.py /usr/local/sbin/zcheck
 install -o root -g root -m 755 zcheck.cron /etc/cron.weekly/zcheck
 install -o root -g root -m 755 packages.cron /etc/cron.weekly/packages
